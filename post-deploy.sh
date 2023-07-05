537d9fd2e10f46f6b4eaef95573d109839ba8165
jreasoner-wpe <116589164+jreasoner-wpe@users.noreply.github.com>
2023-07-05 13:12:53 -0400

    Update post-deploy.sh (#19)
    * Update post-deploy.sh
    
    * Update readme.md
    
    * update links
    
    * Update readme.md
    
    Co-authored-by: Justin Prukop <prukop@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Justin Prukop <prukop@users.noreply.github.com>

--- post-deploy.sh
+++ post-deploy.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-filename=deploy_log.txt
-echo -e "$(date): \$ wp acorn view:cache\n$(wp acorn view:cache 2>&1)" >> $filename
-
-# View your deploy log
-# https://[your-wp-install-name].wpengine.com/deploy_log.txt
+wp acorn view:cache
